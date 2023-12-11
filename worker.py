109fb84f60c9fb3df9db0dc842a948de33cda712
Paul <C21359216@mytudublin.ie>
2023-12-11 19:02:31 +0000

    e

--- worker.py
+++ worker.py
@@ -9,10 +9,6 @@ def get_api_key() -> str:
     secret = os.environ.get("COMPUTE_API_KEY")
     if secret:
         return secret
-    else:
-        #local testing
-        with open('.key') as f:
-            return f.read()
       
 @app.route("/")
 def hello():
